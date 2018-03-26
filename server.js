cefad5b01f973372ad3780e076e4d638c26e9047
Santeri Rusila <santtu_1993@hotmail.com>
2018-03-26 09:38:36 +0300

    Update

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.get('/stream', function(req, res) {
   connections.push(res);
 });
 
-app.listen(3200, function() {
-  console.log('Listening on port 3200...')
+app.listen(3201, function() {
+  console.log('Listening on port 3201...')
 });
